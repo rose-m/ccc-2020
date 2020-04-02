99fbf4cf30f8b4be754c9755c2035ea714f704d1
rosem <michael_rose@gmx.de>
2020-04-02 10:22:07 +0200

    cleanup

--- src/main/java/io/coderose/ccc2020/Main.java
+++
@@ -1,9 +0,0 @@
-package io.coderose.ccc2020;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello CCC 2020!");
-    }
-
-}
---
+++ src/main/java/io/coderose/ccc2020/challenges/Challenge1.java
@@ -0,0 +1,9 @@
+package io.coderose.ccc2020.challenges;
+
+public class Challenge1 {
+
+    public static void main(String[] args) {
+        System.out.println("Ready to go");
+    }
+
+}

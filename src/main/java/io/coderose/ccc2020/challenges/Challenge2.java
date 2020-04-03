48864b166a312246396d8af8d0586fead9b90447
Christian Semrau <christian.semrau@collaboration-factory.de>
2020-04-03 15:37:15 +0200

    challenge 2 all solutions

--- src/main/java/io/coderose/ccc2020/challenges/Challenge2.java
+++ src/main/java/io/coderose/ccc2020/challenges/Challenge2.java
@@ -1,6 +1,5 @@
 package io.coderose.ccc2020.challenges;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Table;
 import com.google.common.collect.TreeBasedTable;
 import io.coderose.ccc2020.utilities.FileReader;
@@ -17,34 +16,36 @@ public class Challenge2 extends AbstractChallenge {
 
     @Override
     public void run() {
-        reader = newReader("level2_1.in");
+        for (int f = 1; f <= 5; f++) {
+            reader = newReader("level2_" + f + ".in");
+            File file = new File("level2_" + f + ".out");
 
             PrintWriter w;
-        try {
-            File file = new File("level2_1.out");
-            System.out.println(file.getAbsoluteFile());
-            w = new PrintWriter(new FileWriter(file));
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
+            try {
+                System.out.println(file.getAbsoluteFile());
+                w = new PrintWriter(new FileWriter(file));
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
+
+            final Integer numLines = reader.parseInts().get(0);
+
+            Table<String, String, Integer> flightCounts = TreeBasedTable.create();
+
+            for (int l = 0; l < numLines; l++) {
+                final List<String> lineValues = reader.parseStrings();
+                String source = lineValues.get(4);
+                String target = lineValues.get(5);
+                Integer count = flightCounts.get(source, target);
+                count = (count == null ? 1 : count + 1);
+                flightCounts.put(source, target, count);
+            }
+
+            for (Table.Cell<String, String, Integer> cell : flightCounts.cellSet()) {
+                w.println(cell.getRowKey() + " " + cell.getColumnKey() + " " + cell.getValue());
+            }
+            w.close();
         }
-
-        final Integer numLines = reader.parseInts().get(0);
-
-        Table<String, String, Integer> flightCounts = TreeBasedTable.create();
-
-        for (int l = 0; l < numLines; l++) {
-            final List<String> lineValues = reader.parseStrings();
-            String source = lineValues.get(4);
-            String target = lineValues.get(5);
-            Integer count = flightCounts.get(source, target);
-            count = (count == null ? 1 : count + 1);
-            flightCounts.put(source, target, count);
-        }
-
-        for (Table.Cell<String, String, Integer> cell : flightCounts.cellSet()) {
-            w.println(cell.getRowKey() + " " + cell.getColumnKey() + " " + cell.getValue());
-        }
-        w.close();
     }
 
     public static void main(String[] args) {
